4d60db2288538faff7a1bf21c84fb9a113fab1fb
Dudeman <seanpsemyck98@gmail.com>
2025-11-08 22:23:25 -0600

    transfering _toggleButtonState & _checkInputValidity

--- components/FormValidation.js
+++ components/FormValidation.js
@@ -8,8 +8,43 @@ class FormValidator {
     this._formEl = formEl;
   }
 
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      showInputError(
+        this._formEl,
+        inputElement,
+        inputElement.validationMessage,
+        settings
+      );
+    } else {
+      hideInputError(inputElement);
+    }
+  }
+
+  _toggleButtonState() {
+    if (hasInvalidInput(this._inputList)) {
+      buttonEl.classList.add(this._inactiveButtonClass);
+      buttonEl.disabled = true;
+    } else {
+      buttonEl.classList.remove(this._inactiveButtonClass);
+      buttonEl.disabled = false;
+    }
+  }
+
   _setEventListeners() {
-    console.log("success");
+    this._inputList = Array.from(
+      this._formEl.querySelectorAll(this._inputSelector)
+    );
+    const buttonEl = this._formEl.querySelector(this._submitButtonSelector);
+
+    _toggleButtonState(inputList, buttonEl, settings);
+
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._checkInputValidity(inputElement);
+        _toggleButtonState(inputList, buttonEl, settings);
+      });
+    });
   }
 
   enableValidation() {
